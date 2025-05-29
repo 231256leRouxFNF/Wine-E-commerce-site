a64c158b9caf388a96239e6c246e8be294f5297b
OpenWindow231312 <231312@virtualwindow.co.za>
2025-05-29 12:01:22 +0200

    about hero banner and styling about page

--- e-wine/src/App.js
+++ e-wine/src/App.js
@@ -1,19 +1,20 @@
 // App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from '@mui/material/styles';
-import { CssBaseline } from '@mui/material';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
 
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 
-import Home from './pages/Home';
-import Products from './pages/Products';
-import LoginPage from './pages/LoginPage';
-import Register from './pages/Register';  // Added this import
-import Cart from './pages/Cart';
-import AddProduct from './pages/AddProduct';
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import LoginPage from "./pages/LoginPage";
+import Register from "./pages/Register";
+import Cart from "./pages/Cart";
+import AddProduct from "./pages/AddProduct";
+import About from "./pages/About"; // ✅ Confirmed import
 
-import theme from './theme';
+import theme from "./theme";
 
 function App() {
   return (
@@ -22,12 +23,13 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/add-product" element={<AddProduct />} />
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
+          <Route path="/about" element={<About />} />{" "}
           <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<Register />} />  {/* Add this route */}
+          <Route path="/register" element={<Register />} />
+          <Route path="/add-product" element={<AddProduct />} />
         </Routes>
       </Router>
     </ThemeProvider>
